43c1fc4b3887a2d7020085e76fdbd64a1c58e385
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-01-21 18:27:40 +0900

    use vuex

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import "@/styles/core/fonts.scss";
 import "@/styles/core/reset.scss";
 import "@/styles/core/base.scss";
 
+import store from "./store";
+
 export default function (Vue, { router, head, isClient, appOptions }) {
-  // appOptions.store = store;
+  appOptions.store = store;
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
